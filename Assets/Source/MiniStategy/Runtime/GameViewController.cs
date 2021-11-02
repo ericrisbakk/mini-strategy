0fe47615256882e1d1c0d37ba1a3a4a6f7c5e0e9
Risbakk <eric.risbakk@gmail.com>
2021-11-02 18:18:23 +0100

    Created `GameViewController` class.

---
+++ Assets/Source/MiniStategy/Runtime/GameViewController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Source.MiniStategy.Runtime;
+using UnityEngine;
+
+/// <summary>
+/// Responsible for visualizing the game state
+/// </summary>
+public class GameViewController : MonoBehaviour {
+    private GameState state;
+}
