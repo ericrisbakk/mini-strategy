c76379d16794f5fd1f2f24aa82a0ae1e014c7b6c
Risbakk <eric.risbakk@gmail.com>
2021-11-02 14:26:29 +0100

    Move basic game objects into their own `Object` folder.

--- Assets/Source/MiniStategy/Runtime/Objects/Piece.cs
+++ Assets/Source/MiniStategy/Runtime/Objects/Piece.cs
@@ -1,8 +1,17 @@
+using Source.StrategyFramework.Runtime;
+
 namespace Source.MiniStategy.Runtime {
     /// <summary>
     /// Anything which exists in a single location on a map.
     /// </summary>
     public interface IPiece {
+        
+    }
+
+    /// <summary>
+    /// Data definition for piece.
+    /// </summary>
+    public class Piece : StrategyEntity, IPiece {
         
     }
 }
