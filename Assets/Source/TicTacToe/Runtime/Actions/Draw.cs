e384a7977d75a78f0707ace8ab54b9b733d6fe39
Risbakk <eric.risbakk@gmail.com>
2021-11-19 13:49:34 +0100

    Add `Actions` folder with `Draw` action.

---
+++ Assets/Source/TicTacToe/Runtime/Actions/Draw.cs
@@ -0,0 +1,17 @@
+using Source.TicTacToe.Runtime.Objects;
+using UnityEngine;
+
+namespace Source.TicTacToe.Runtime.Actions {
+    /// <summary>
+    /// A specific player tries to draw their symbol in the given position.
+    /// </summary>
+    public class Draw {
+        public Vector2Int Position;
+        public Player Player;
+
+        public Draw(Vector2Int position, Player player) {
+            Position = position;
+            Player = player;
+        }
+    }
+}
