ec76d1c266931eadd5115fc66b0643bba5e70a18
Risbakk <eric.risbakk@gmail.com>
2021-12-08 12:56:09 +0100

    `Steps`: Removed checking for EnPassant. Fixed up current validation of the `MoveStep`.

--- Assets/Source/Chess/Runtime/Steps.cs
+++ Assets/Source/Chess/Runtime/Steps.cs
@@ -20,8 +20,7 @@ namespace Source.Chess.Runtime {
                 state.Squares()[t.x, t.y] = move.Piece;
                 return state;
             }
-            public abstract GameState ValidateForward(GameState state);
-
+            
             public virtual GameState Backward(GameState state) {
                 var s = move.Source;
                 var t = move.Target;
@@ -29,26 +28,43 @@ namespace Source.Chess.Runtime {
                 state.Squares()[t.x, t.y] = move.Capture;
                 return state;
             }
-            public abstract GameState ValidateBackward(GameState state);
 
-            public GameState CommonForwardValidation(GameState state) {
-                var s = move.Source;
-                var t = move.Target;
+            private GameState CommonValidation(GameState state) {
                 Assert.AreNotEqual(move.Player.Color, PlayerType.Unassigned, 
                     "Player must have a color.");
                 Assert.IsTrue(Rules.OwnsPiece(move.Player, move.Piece),
                     "Player and piece color do not match.");
-                Assert.IsTrue(state.Squares()[s.x, s.y] == move.Piece,
-                    "The piece on source position must match the piece value of the move.");
-                Assert.IsTrue(state.Squares()[t.x, t.y] != PieceType.OutOfBounds,
-                    "Move target cannot be out of bounds.");
                 Assert.IsTrue(move.Capture != PieceType.OutOfBounds,
                     "Out of bounds cannot be captured!");
-                Assert.IsTrue(state.Squares()[t.x, t.y] == move.Capture,
-                    "The piece on target position must match the capture value of the move.");
                 if (Rules.MoveCaptures(move))
                     Assert.IsTrue(Rules.OwnsPiece(Rules.GetOtherPlayer(state, move.Player), move.Capture), 
                         "The piece captured must belong to the other player.");
+                return state;
+            }
+            
+            public virtual GameState ValidateBackward(GameState state) {
+                CommonValidation(state);
+                var s = move.Source;
+                var t = move.Target;
+                Assert.IsTrue(state.Squares()[t.x, t.y] == move.Piece,
+                    "[Backward] Target piece does not match piece of move.");
+                Assert.IsTrue(state.Squares()[s.x, s.y] == PieceType.Empty,
+                    "[Backward] Source must be empty.");
+
+                return state;
+            }
+
+            public virtual GameState ValidateForward(GameState state) {
+                CommonValidation(state);
+                var s = move.Source;
+                var t = move.Target;
+                Assert.IsTrue(state.Squares()[s.x, s.y] == move.Piece,
+                    "Source piece must match the piece value of the move.");
+                Assert.IsTrue(state.Squares()[t.x, t.y] != PieceType.OutOfBounds,
+                    "Move target cannot be out of bounds.");
+                Assert.IsTrue(state.Squares()[t.x, t.y] == move.Capture,
+                    "The piece on target position must match the capture value of the move.");
+
                 return state;
             }
         }
@@ -57,24 +73,8 @@ namespace Source.Chess.Runtime {
         public class PawnMoveStep : MoveStep {
             public PawnMoveStep(Move move) : base(move) {}
 
-            public override GameState Forward(GameState state) {
-                var s = move.Source;
-                var t = move.Target;
-                state.Squares()[s.x, t.y] = PieceType.Empty;
-                return base.Forward(state);
-            }
-
-            public override GameState Backward(GameState state) {
-                var s = move.Source;
-                var t = move.Target;
-                // TODO: OOOF! En Passant will be incorrect.
-                // The base `Backward` sets target to capture, but `En passant` is not captured.
-                // ALSO: En passant can only be done immediately after another player
-                // moves their pawn two spaces forward
-                return base.Backward(state);
-            }
-
             public override GameState ValidateForward(GameState state) {
+                base.ValidateForward(state);
                 var s = move.Source;
                 var t = move.Target;
                 var start = move.Player.Color == PlayerType.White ? 7 : 2;
@@ -83,21 +83,16 @@ namespace Source.Chess.Runtime {
                 Assert.IsTrue(direction == Math.Sign(t.x - s.x),
                     "Pawn can only move towards opposite side.");
                 
-                if (Rules.MoveCaptures(move)) {
+                if (Rules.MoveCaptures(move))
                     Assert.IsTrue(Math.Abs(t.y - s.y) == 1
                                   && Math.Abs(t.x - s.x) == 1,
                         "pawn can only ever capture by moving diagonally.");
-                    
-                    if (state.Squares()[t.x, t.y] == PieceType.Empty) {
-                        Assert.IsTrue(state.Squares()[s.x, t.y] == move.Capture,
-                            "En passant detected, but the captured piece did not match.");
-                    }
-                }
-                
+
                 if (Math.Abs(t.x - s.x) == 2)
                     Assert.IsTrue(s.x == start 
-                                  && Math.Abs(t.y - s.y) == 0,
-                    "Pawn can only move forward two spaces when at starting position");
+                                  && Math.Abs(t.y - s.y) == 0
+                                  && state.Squares()[s.x + direction, s.y] == PieceType.Empty,
+                        "Pawn can only move forward two spaces when at starting position and unblocked.");
 
 
                 return state;
