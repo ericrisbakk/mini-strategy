7382613126d286401c55971ce3781df2cf500650
Risbakk <eric.risbakk@gmail.com>
2021-12-13 15:30:22 +0100

    Added some utility methods to the GameState.

--- Assets/Source/Chess/Runtime/GameState.cs
+++ Assets/Source/Chess/Runtime/GameState.cs
@@ -1,11 +1,14 @@
 using Source.Chess.Runtime.Objects;
 using Source.StrategyFramework.Runtime.Representation;
+using UnityEngine;
 
 namespace Source.Chess.Runtime {
     public class GameState : IState {
         public Player White { get; }
         public Player Black { get; }
         public Board Board { get; }
+        
+        public Player CurrentPlayer { get; set; }
 
         public GameState() {
             White = new Player(PlayerType.White);
@@ -14,5 +17,7 @@ namespace Source.Chess.Runtime {
         }
 
         public PieceType[,] Squares() => Board.Squares;
+        public PieceType Square(int x, int y) => Board.Squares[x, y];
+        public PieceType Square(Vector2Int pos) => Board.Squares[pos.x, pos.y];
     }
 }
