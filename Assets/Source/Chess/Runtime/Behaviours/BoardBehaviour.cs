afba16aac8898ed69af51b05bfc9e34164162b92
Risbakk <eric.risbakk@gmail.com>
2021-12-22 09:51:20 +0100

    Basic chess board script - I want to be able to highlight squares as well.

--- Assets/Source/Chess/Runtime/Behaviours/BoardBehaviour.cs
+++ Assets/Source/Chess/Runtime/Behaviours/BoardBehaviour.cs
@@ -1,10 +1,27 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Source.Chess.Runtime.Behaviours {
     public class BoardBehaviour : MonoBehaviour {
-        public GameObject whiteSquare;
-        public GameObject blackSquare;
-        
-        
+        public GameObject square;
+
+        public Colors whiteColors;
+        public Colors blackColors;
+
+        [Button]
+        public void GenerateBoard() {
+            for (int i = transform.childCount; i > 0; i--) {
+                DestroyImmediate(transform.GetChild(0).gameObject);
+            }
+
+            for (int i = 0; i < 64; i++) {
+                var s = Instantiate(square, transform);
+                var sBehaviour = s.GetComponent<SquareBehaviour>();
+                var oddRow = (i / 8) % 2 == 0;
+                var even = i % 2 == 0;
+                sBehaviour.Colors = (oddRow && !even) || (!oddRow && even) ? blackColors : whiteColors;
+                sBehaviour.ClearHighlight();
+            }
+        }
     }
 }
