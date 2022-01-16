73c0faba9520b07a000b9069837edcfd41361f59
Risbakk <eric.risbakk@gmail.com>
2022-01-16 18:00:19 +0100

    Expanded `EnPassantStep` forward validation, added `EnPassant` func which derives the capture position.

--- Assets/Source/Chess/Runtime/Steps/EnPassantStep.cs
+++ Assets/Source/Chess/Runtime/Steps/EnPassantStep.cs
@@ -1,6 +1,7 @@
 using Source.Chess.Runtime.Actions;
 using Source.Chess.Runtime.Objects;
 using Source.StrategyFramework.Runtime.Representation;
+using UnityEngine.Assertions;
 
 namespace Source.Chess.Runtime.Steps {
     public class EnPassantStep : IStep<GameState> {
@@ -13,12 +14,12 @@ namespace Source.Chess.Runtime.Steps {
         public GameState Forward(GameState state) {
             var s = EnPassant.Source;
             var t = EnPassant.Target;
+            var c = EnPassant.Capture;
             var color = EnPassant.Player.Color;
-            var direction = Rules.GetPawnDirection(color);
 
             state.Squares()[s.x, s.y] = PieceType.Empty;
             state.Squares()[t.x, t.y] = color == Color.White ? PieceType.WPawn : PieceType.BPawn;
-            state.Squares()[t.x - direction, t.y] = PieceType.Empty;
+            state.Squares()[c.x, c.y] = PieceType.Empty;
 
             return state;
         }
@@ -26,22 +27,41 @@ namespace Source.Chess.Runtime.Steps {
         public GameState Backward(GameState state) {
             var s = EnPassant.Source;
             var t = EnPassant.Target;
+            var c = EnPassant.Capture;
             var color = EnPassant.Player.Color;
-            var direction = Rules.GetPawnDirection(color);
 
             state.Squares()[s.x, s.y] = color == Color.White ? PieceType.WPawn : PieceType.BPawn;
             state.Squares()[t.x, t.y] = PieceType.Empty;
-            state.Squares()[t.x - direction, t.y] = color == Color.White ? PieceType.BPawn : PieceType.WPawn;
+            state.Squares()[c.x, c.y] = color == Color.White ? PieceType.BPawn : PieceType.WPawn;
 
             return state;
         }
 
+        // TODO: Historical validation - this move can only be done if the captured pawn recently moved.
         public GameState ValidateForward(GameState state) {
+            CommonValidation(state);
+            var squares = state.Squares();
+            var s = EnPassant.Source;
+            var t = EnPassant.Target;
+            var c = EnPassant.Capture;
+
+            StepValidation.OwnsPiece(EnPassant.Player, state.Squares()[s.x, s.y]);
+            StepValidation.PositionIsPiece(state.Squares(), s, new [] {PieceType.WPawn, PieceType.BPawn});
+            StepValidation.PositionIsPiece(state.Squares(), c, new [] {PieceType.WPawn, PieceType.BPawn});
+            StepValidation.OpposingPieces(squares[s.x, s.y], squares[c.x, c.y]);
+            StepValidation.PositionIsPiece(state.Squares(), t, PieceType.Empty);
             throw new System.NotImplementedException();
         }
 
         public GameState ValidateBackward(GameState state) {
+            CommonValidation(state);
             throw new System.NotImplementedException();
+        }
+
+        private GameState CommonValidation(GameState state) {
+            StepValidation.PlayerColorAssigned(EnPassant.Player);
+
+            return state;
         }
     }
 }
