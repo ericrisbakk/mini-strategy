68706289aa87d268f778c59a1bf018ef45bd8ffa
Risbakk <eric.risbakk@gmail.com>
2022-01-20 13:07:17 +0100

    Begin on `TestPawnActions`.

--- Assets/Source/Chess/Tests/Runtime/TestPawnActions.cs
+++ Assets/Source/Chess/Tests/Runtime/TestPawnActions.cs
@@ -1,5 +1,21 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Source.Chess.Runtime;
+using UnityEngine;
+using static Source.Chess.Tests.Runtime.TestUtility;
+
 namespace Source.Chess.Tests.Runtime {
     public class TestPawnActions {
-        
+        [Test]
+        public void TestPawnStartMove() {
+            var white = "b2";
+            var black = "g7";
+            var state = new GameState(white, black);
+
+            var expected = new List<Tuple<Vector2Int, PieceType>>() {
+                
+            };
+        }
     }
 }
