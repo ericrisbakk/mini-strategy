dbf3fd996b34a173dbceda43650525ab3e18893b
Risbakk <eric.risbakk@gmail.com>
2022-01-25 11:33:58 +0100

    Moved methods from `PawnActionsTest` to `TestUtility`.

--- Assets/Source/Chess/Tests/Runtime/PawnActionsTest.cs
+++ Assets/Source/Chess/Tests/Runtime/PawnActionsTest.cs
@@ -6,9 +6,8 @@ using Source.Chess.Runtime.Actions;
 using Source.Chess.Runtime.Objects;
 using Source.StrategyFramework.Runtime.History;
 using Source.StrategyFramework.Runtime.Representation;
-using UnityEngine;
 using static Source.Chess.Runtime.Rules;
-using Assert = UnityEngine.Assertions.Assert;
+using static Source.Chess.Tests.Runtime.TestUtility;
 
 namespace Source.Chess.Tests.Runtime {
     public class PawnActionsTest {
@@ -23,20 +22,24 @@ namespace Source.Chess.Tests.Runtime {
             var history = new LinearHistory();
 
             state.CurrentPlayer = state.White;
-            var expected = new List<IAction>() {
-                Add(state.CurrentPlayer, PieceType.WPawn, "b2", PieceType.Empty, "b3"),
-                Add(state.CurrentPlayer, PieceType.WPawn, "b2",PieceType.Empty,"b4")
+            var whiteTests = new List<Tuple<string, List<IAction>>>() {
+                new Tuple<string, List<IAction>>("b2", new List<IAction>() {
+                    Add(state.CurrentPlayer, PieceType.WPawn, "b2", PieceType.Empty, "b3"),
+                    Add(state.CurrentPlayer, PieceType.WPawn, "b2",PieceType.Empty,"b4")
+                })
             };
-            var actions = GetActions(state, history, ToVector2Int('2', 'b'));
-            AssertActionsEqualAndUnique(actions, expected);
+            CompareActions(state, history, whiteTests);
+            CompareAllActions(state, history, whiteTests);
 
             state.CurrentPlayer = state.Black;
-            expected = new List<IAction>() {
-                Add(state.CurrentPlayer, PieceType.WPawn, "g7", PieceType.Empty, "g6"),
-                Add(state.CurrentPlayer, PieceType.WPawn, "g7",PieceType.Empty,"g5")
+            var blackTests = new List<Tuple<string, List<IAction>>>() {
+                new Tuple<string, List<IAction>>("g7", new List<IAction>() {
+                    Add(state.CurrentPlayer, PieceType.WPawn, "g7", PieceType.Empty, "g6"),
+                    Add(state.CurrentPlayer, PieceType.WPawn, "g7",PieceType.Empty,"g5")
+                })
             };
-            actions = GetActions(state, history, ToVector2Int('7', 'g'));
-            AssertActionsEqualAndUnique(actions, expected);
+            CompareActions(state, history, blackTests);
+            CompareAllActions(state, history, blackTests);
         }
 
         [Test]
@@ -80,60 +83,6 @@ namespace Source.Chess.Tests.Runtime {
             
             CompareActions(state, history, blackTests);
             CompareAllActions(state, history, blackTests);
-        }
-
-        private void CompareActions(GameState state, LinearHistory history, List<Tuple<string, List<IAction>>> tests) {
-            foreach (var t in tests) {
-                var rank = t.Item1[1];
-                var file = t.Item1[0];
-                var actions = GetActions(state, history, ToVector2Int(rank, file));
-                AssertActionsEqualAndUnique(actions, t.Item2);
-            }
-        }
-
-        private void CompareAllActions(GameState state, LinearHistory history,
-            List<Tuple<string, List<IAction>>> tests) {
-            var expected = new List<IAction>();
-            foreach (var tuple in tests) {
-                expected.AddRange(tuple.Item2);
-            }
-
-            var allActions = GetAllActions(state, history);
-            AssertActionsEqualAndUnique(allActions, expected);
-        }
-
-        
-        private void AssertActionsEqualAndUnique(List<IAction> actions, List<IAction> expected) {
-            Assert.IsTrue(actions.Count == expected.Count,
-                "Number of actions generated does not match number of expected actions.");
-            
-            int[] counts = new int[actions.Count];
-            
-            for (int i = 0; i < actions.Count; i++) {
-                for (int j = 0; j < expected.Count; j++) {
-                    if (Equals(actions[i], expected[j]))
-                        counts[i] += 1;
-                }
-            }
-            
-            for (int i = 0; i < actions.Count; i++) {
-                var c = counts[i];
-                var a = actions[i];
-                Assert.IsTrue(c == 1,
-                    $"Generated action {i}: {a} had {c} matches, but there should only be one match.");
-            }
-        }
-
-        private bool Equals(IAction a1, IAction a2) {
-            if (a1.GetType() != a2.GetType())
-                return false;
-            if (a1 is Move m && m.Equals((Move) a2))
-                return true;
-            if (a1 is EnPassant ep && ep.Equals((EnPassant) a2))
-                return true;
-            if (a1 is Promote p && p.Equals((Promote) a2))
-                return true;
-            return false;
         }
     }
 }
