beaecb8843b6f1d3f035ff7760cfb1a1bdf41bc2
Risbakk <eric.risbakk@gmail.com>
2022-02-02 14:02:33 +0100

    Begin work on highlighting in `BoardBehaviour`.

--- Assets/Source/Chess/Runtime/Behaviours/BoardBehaviour.cs
+++ Assets/Source/Chess/Runtime/Behaviours/BoardBehaviour.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 using Sirenix.OdinInspector;
-using Sirenix.Serialization;
 using UnityEngine;
 
 namespace Source.Chess.Runtime.Behaviours {
+    public enum HighlightType {
+        Selected,
+        Move,
+        Capture,
+        Castle,
+        EnPassant,
+        Promotion,
+        Check,
+        Checkmate
+    }
+    
     public class BoardBehaviour : SerializedMonoBehaviour {
 
         #region Variables
@@ -12,13 +23,72 @@ namespace Source.Chess.Runtime.Behaviours {
 
         public Colors whiteColors;
         public Colors blackColors;
+        public Dictionary<HighlightType, Colors> HighlightColorsDict = new Dictionary<HighlightType, Colors>();
         public SquareBehaviour[,] Squares;
-        
+
+        #region State
+
         [NonSerialized] public GameObject[,] Pieces = new GameObject[8,8];
         
+        public Vector2Int clickedTarget;
+        public bool clicked;
+        
+        #endregion
+
         #endregion
         
+        private void Awake() {
+            foreach (var s in Squares) {
+                s.OnPointerClick -= OnPointerClick;
+                s.OnPointerClick += OnPointerClick;
+                s.OnPointerEnter -= OnPointerEnter;
+                s.OnPointerEnter += OnPointerEnter;
+                s.OnPointerExit -= OnPointerExit;
+                s.OnPointerExit += OnPointerExit;
+            }
+        }
+
+            // TODO: Clicking on somewhere not highlighted while having clicked should set that to the new target.
+            private void OnPointerClick(Vector2Int target) {
+            if (clicked) {
+                if (target == clickedTarget) {
+                    clicked = false;
+                }
+                else {
+                    // clickedTarget = target;
+                }
+            }
+            else {
+                clicked = true;
+                clickedTarget = target;
+            }
+        }
+
+        private void OnPointerEnter(Vector2Int target) {
+            if (!clicked) { 
+                Highlight(target);
+            }
+        }
         
+        private void OnPointerExit(Vector2Int target) {
+            if (!clicked) { 
+                ClearHighlight();
+            }
+        }
+
+        private void Highlight(Vector2Int target) {
+            var s = Squares[target.x, target.y];
+            s.Highlight(HighlightColorsDict[HighlightType.Selected],
+                true, true, true, true, true);
+        }
+
+        private void ClearHighlight() {
+            foreach (var s in Squares) {
+                s.ClearHighlight();
+            }
+        }
+
+
 #if UNITY_EDITOR
         [Button(ButtonSizes.Medium)]
         public void GenerateBoard() {
